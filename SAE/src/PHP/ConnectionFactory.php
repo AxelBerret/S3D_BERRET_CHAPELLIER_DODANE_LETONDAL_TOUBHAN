d5e367a528c42b4e9036242967af8e6d458ea3d8
Titoufeur <titouan.letondal@gmail.com>
2023-11-07 11:02:30 +0100

    Ajout ConnectionFactory

---
+++ SAE/src/PHP/ConnectionFactory.php
@@ -0,0 +1,31 @@
+<?php
+class ConnectionFactory{
+    private static $config;
+    private static $pdo;
+    public static function setConfig($configName){
+        self::$config = parse_ini_file($configName);
+        $dsn = "'{$config['driver']}:host={$config['host']};dbname={$config['database']};charset=utf8'";
+        $username = self::$config['username'];
+        $password = $config['password'];
+    }
+
+    public static function makeConnection(){
+        if (!self::$config) {
+            throw new Exception('La configuration n\'a pas été définie. Utilisez setConfig($configFileName) pour la définir.');
+        }
+        if (!self::$pdo) {
+            $dsn = self::$config['driver'] . ':host=' . self::$config['host'] . ';dbname=' . self::$config['database'] . ';charset=utf8';
+            $username = self::$config['username'];
+            $password = self::$config['password'];
+
+            try {
+                self::$pdo = new PDO($dsn, $username, $password);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                throw new Exception('Erreur de connexion à la base de données : ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?>
