edb2ac9edc01d6c22712b9db71cf9367a96f2729
fabien.toubhans7@etu.univ-lorraine.fr <fabien.toubhans7@etu.univ-lorraine.fr>
2023-11-07 10:58:36 +0100

    MAJ de mes classes

--- SAE/src/index.php
+++ SAE/src/index.php
@@ -1,32 +1,7 @@
 <?php
 
-require 'Signup.php';
+require '/PHP/Autoloader.php';
 
-//Si le formulaire de connexion ou signup est fourni
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    try {
-
-        $db = new PDO('mysql:host=localhost;dbname=SAE_touiteur;charset=utf8', 'root', '');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $signup = new Signup($db);
-
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        if ($signup->signup($email, $password)) {
-            // Si l'inscription on redirige vers confirmation.php (à voir plus tard si la méthode header marche bien on l'a pas vu en cours
-            header('Location: confirmation.php');
-            exit;
-        }
-        //Ici les eventuelles erreur liées au signup de l'utilisateur
-    } catch (DoublonEmailException $e) {
-        $errorMessage = $e->getMessage();
-    } catch (MailInvalideException $e) {
-        $errorMessage = $e->getMessage();
-    } catch (MDPNonRobusteException $e) {
-        $errorMessage = $e->getMessage();
-    }
-}
+//Main global
 
 ?>
