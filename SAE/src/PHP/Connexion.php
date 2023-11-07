11acc1acc5be47e2e51475b4a0b840c834254e3f
fabien.toubhans7@etu.univ-lorraine.fr <fabien.toubhans7@etu.univ-lorraine.fr>
2023-11-07 09:38:37 +0100

    Modification des directories pour correspondre à l'autoloader également rajouté

---
+++ SAE/src/PHP/Connexion.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+class Connexion{
+
+    private $db;
+
+    public function __construct(PDO $db) {
+        $this->db = $db;
+    }
+
+
+        public function login($email, $password)
+    {
+        // On utilise des requêtes préparées afin de prévenir les injections SQL
+        $query = "SELECT * FROM Utilisateur  WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            // Ici, on utilise la fonction php password_verify pour vérifier le mot de passe hashé
+            return true;
+        }
+        return false;
+    }
+
+}
+
+?>
