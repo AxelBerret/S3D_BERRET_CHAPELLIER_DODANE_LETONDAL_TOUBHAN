259b10b393138c455ef9b98eedd61564ed357f46
fabien.toubhans7@etu.univ-lorraine.fr <fabien.toubhans7@etu.univ-lorraine.fr>
2023-11-08 15:26:09 +0100

    Mise en marche des inscriptions directement sur le HTML

--- SAE/src/PHP/Signup.php
+++ SAE/src/PHP/Signup.php
@@ -57,6 +57,7 @@ class Signup{
             $_SESSION['user_id'] = $idutilisateur;
             $_SESSION['nom'] = $nom;
             $_SESSION['prenom'] = $prenom;
+
             return true;
         } else {
             //Dans le cas ou false est retourné, soit il y'a un problème dans la BDD soit un problème dans le nom ou le prénom
