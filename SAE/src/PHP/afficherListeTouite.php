ad375c81711d487884a450e30a064484f4cd615a
Titoufeur <titouan.letondal@gmail.com>
2023-11-10 14:44:26 +0100

    Update afficherListeTouite.php

--- SAE/src/PHP/afficherListeTouite.php
+++ SAE/src/PHP/afficherListeTouite.php
@@ -176,7 +176,7 @@ HTML;
 
                 <div class="tendances-container">
                     <div class="tendance-title">Tendances France</div>
-                    <a href="#tag1" class="tag">#Tag1</a>
+                    <a href="dispatcher.php?action=afficherTouitesTag&tag=Tag1" class="tag">#Tag1</a>
                     <a href="#tag2" class="tag">#Tag2</a>
                     <a href="#tag3" class="tag">#Tag3</a>
                 </div>
